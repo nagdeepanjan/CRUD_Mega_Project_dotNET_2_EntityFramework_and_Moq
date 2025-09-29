ad7b21a1e5eb6d9ebb4026d375edb5134f0285bf
nagdeepanjan <mail@deepanjannag.com>
2025-09-29 13:32:48 -0400

    Refactor JSON handling and add data seeding
    Updated the `.csproj` to change JSON file inclusion from `Content` to `None`, indicating they are part of the project but not processed as content files. Populated `countries.json` and `persons.json` with data for database seeding. Enhanced `DeepDbContext` with a constructor for `DbContextOptions` and added seeding logic in `OnModelCreating` to populate `Countries` and `Persons` tables using JSON data.

--- Entities/DeepDbContext.cs
+++ Entities/DeepDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,11 @@ namespace Entities
 {
     public class DeepDbContext: DbContext
     {
+        public DeepDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
         public DbSet<Country> Countries { get; set; }
         public DbSet<Person> Persons { get; set; }
 
@@ -18,6 +24,21 @@ namespace Entities
 
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //Seed to Countries
+            string countriesJson = File.ReadAllText("countries.json");
+            List<Country> countries = JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            foreach (Country country in countries)
+                modelBuilder.Entity<Country>().HasData(country);
+
+
+            //Seed to Persons
+            string personsJson = File.ReadAllText("persons.json");
+            List<Person> persons = JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            foreach (Person person in persons)
+                modelBuilder.Entity<Person>().HasData(person);
         }
     }
 }
